c9076871c7faf676e49defab4481aa98179ca690
Vasileios Mpletsos <msa17020@gmail.com>
2025-10-24 17:29:13 +0300

    Correct visualization

--- vbgrad/vbengine.py
+++ vbgrad/vbengine.py
@@ -1,14 +1,21 @@
+from .visualization_utils import draw_graph
+
+
 class Parameter:
-    def __init__(self, value, _children={}, _operation=""):
+    def __init__(self, value, _children={}, _operation="", variable_name=""):
         self.value = value
         self._children = _children
         self._operation = _operation
+        self.variable_name = variable_name
 
     def __repr__(self):
-        return f"Parameter = {self.value}"
+        return f"{self.variable_name} | Parameter = {self.value}"
 
     def __add__(self, other):
         return Parameter(self.value + other.value, {self, other}, "+")
 
     def __mul__(self, other):
         return Parameter(self.value * other.value, {self, other}, "*")
+
+    def plot_graph(self):
+        return draw_graph(self)
--- vbgrad/visualization_utils.py
+++ vbgrad/visualization_utils.py
@@ -1,12 +1,10 @@
 from graphviz import Digraph
 
-from .vbengine import Parameter
 
-
-def backtrace_connections(root_node: Parameter):
+def backtrace_connections(root_node):
     visited_nodes, edges = set(), set()
 
-    def find_connected_nodes(selected_node: Parameter):
+    def find_connected_nodes(selected_node):
         if selected_node not in visited_nodes:
             visited_nodes.add(selected_node)
             for child in selected_node._children:
@@ -17,13 +15,17 @@ def backtrace_connections(root_node: Parameter):
     return visited_nodes, edges
 
 
-def draw_graph(root_node: Parameter):
+def draw_graph(root_node):
     graph = Digraph(format="svg", graph_attr={"rankdir": "LR"})
     nodes, edges = backtrace_connections(root_node)
 
     for node in nodes:
         uuid = str(id(node))
-        graph.node(name=uuid, label=f"Parameter {node.value}:.4f", shape="box")
+        graph.node(
+            name=uuid,
+            label=f"{node.variable_name} | Parameter {node.value:.4f}",
+            shape="box",
+        )
 
         # Here we create a node just to visualize the operation that took place
         if node._operation:
